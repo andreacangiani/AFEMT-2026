52be6b426243d192ffd06b4e443a95e1ced658bb
Ankur <138376902+Ankur-IIT@users.noreply.github.com>
2026-03-27 14:40:39 +0100

    Update test_mesh.cpp

--- labs/lab01/test_mesh.cpp
+++ labs/lab01/test_mesh.cpp
@@ -1,15 +1,6 @@
-#include <deal.II/dofs/dof_handler.h>
-#include <deal.II/dofs/dof_renumbering.h>
-#include <deal.II/dofs/dof_tools.h>
-
-#include <deal.II/fe/fe_q.h>
-
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_out.h>
 #include <deal.II/grid/tria.h>
-
-#include <deal.II/lac/dynamic_sparsity_pattern.h>
-#include <deal.II/lac/sparse_matrix.h>
 
 #include <iostream>
 
